4b2a51dcf7405511cb16bab49d3059a93590ef44
snl41_000 <snl.410@hotmail.com>
2018-12-17 17:30:48 +0300

    init

---
+++ database_setup.py
@@ -0,0 +1,63 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Brand(Base):
+    __tablename__ = 'brand'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class MackeupItem(Base):
+    __tablename__ = 'mackeup_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    type = Column(String(250))
+    brand_id = Column(Integer, ForeignKey('brand.id'))
+    brand = relationship(Brand)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'type': self.type,
+        }
+
+
+engine = create_engine('sqlite:///mackeupitemtypewithusers.db')
+
+
+Base.metadata.create_all(engine)
---
+++ lotsofmakeup.py
@@ -0,0 +1,56 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Brand, Base, MackeupItem, User
+
+engine = create_engine('sqlite:///mackeupitemtypewithusers.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
+
+
+# Create dummy user
+User1 = User(name="nasser mustafa", email="12346@example.com",
+             picture='www.craftaholicsanonymous.net/wp-content/uploads/2016/01/Tissue-Paper-Flowers-12.jpg')
+session.add(User1)
+session.commit()
+
+# Menu for UrbanBurger
+brand1 = Brand(user_id=1, name="mac")
+
+session.add(brand1)
+session.commit()
+
+mackeup_item1 = MackeupItem(user_id=1, name="red eyeshadow", description="A highly pigmented powder that applies evenly and blends well.",
+                            price="$7.50", type="Eyeshadow", brand=brand1)
+
+session.add(mackeup_item1)
+session.commit()
+
+mackeup_item2 = MackeupItem(user_id=1, name="blue eyeshadow", description="A highly pigmented powder that applies evenly and blends well.",
+                            price="$12.50", type="Eyeshadow", brand=brand1)
+
+session.add(mackeup_item2)
+session.commit()
+
+mackeup_item3 = MackeupItem(user_id=1, name="brown eyeshadow", description="A highly pigmented powder that applies evenly and blends well.",
+                            price="$12.50", type="Eyeshadow", brand=brand1)
+
+session.add(mackeup_item3)
+session.commit()
+
+brand2 = Brand(user_id=1, name="sephora")
+
+session.add(brand2)
+session.commit()
+
+mackeup_item4 = MackeupItem(user_id=1, name="red lipstick", description="A highly pigmented lipstick that applies evenly and blends well.",
+                            price="$12.50", type="Eyeshadow", brand=brand2)
+
+session.add(mackeup_item4)
+session.commit()
+print "added mackeup items!"
